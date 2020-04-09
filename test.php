38310a5d77caaa6bad4d9f70c14a3ac623fd6c0b
shalvah <diakon.ng@gmail.com>
2020-04-09 09:00:23 +0100

    Update test file

--- test.php
+++ test.php
@@ -4,18 +4,18 @@ require 'vendor/autoload.php';
 
 use Shalvah\Clara\Clara;
 
+Clara::info("Installing package");
+Clara::debug("Attempt 3 of 5");
+Clara::warn("The file does not exist.");
+Clara::error("Something went wrong!");
+Clara::success("Done. Go and be awesome.");
 
 Clara::mute();
-Clara::info("Installing package");
-Clara::debug("Attempt 3 of 5");
-Clara::warn("The file does not exist.");
-Clara::error("Something went wrong!");
-Clara::success("Done. Go and be awesome.");
-Clara::info("Installing package");
-Clara::debug("Attempt 3 of 5");
-Clara::warn("The file does not exist.");
-Clara::error("Something went wrong!");
-Clara::success("Done. Go and be awesome.");
+Clara::info("Muted: Installing package");
+Clara::debug("Muted: Attempt 3 of 5");
+Clara::warn("Muted: The file does not exist.");
+Clara::error("Muted: Something went wrong!");
+Clara::success("Muted: Done. Go and be awesome.");
 
 Clara::unmute();
 Clara::info("Unmuted: Installing package");
